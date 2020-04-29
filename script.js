130afdabc2a9544f1c03e6f550f2efcaacebd8d4
karyna4k <karyna.kananovich@gmail.com>
2020-04-29 18:06:14 +0300

    fix

--- script.js
+++ script.js
@@ -35,13 +35,25 @@ function setClock(id, endtime) {
     //updates the clock every second
     function updateClock() {
         let t = getTimeRemaining(endtime);
+
+        //add zero
+        function addZero(num) {
+            if (num <= 9) {
+                return '0' + num;
+            } else return num;
+        }
+
         //replacing the values in the markup
-        hours.textContent = t.hours;
-        minutes.textContent = t.minutes;
-        seconds.textContent = t.seconds;
+        hours.textContent = addZero(t.hours);
+        minutes.textContent = addZero(t.minutes);
+        seconds.textContent = addZero(t.seconds);
+
 
         if (t.total <= 0) {
             clearInterval(timeInterval);
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
         }
     }
 }
